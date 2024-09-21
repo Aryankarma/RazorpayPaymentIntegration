ceca148bbc56037584e3162d790239ce2b6361c4
aryankarma <aryankarma29@gmail.com>
2024-09-21 20:22:49 +0530

    cors update

--- server/server.js
+++ server/server.js
@@ -15,6 +15,9 @@ dotenv.config()
 app.use(
   cors({
     origin: "https://razorpay-payment-integration.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
